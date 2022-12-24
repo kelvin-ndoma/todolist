2ecf59bd710c0489aa5393afc9504a955351b753
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-24 14:54:50 +0300

    added a delete button and few event listeners

--- index.js
+++ index.js
@@ -28,3 +28,44 @@ function addImage(img_url){
  
 }
 addImage('image/one.jpg')// added A  new image
+
+// adding evendoc
+document.querySelector('form').addEventListener
+('mouseover',e => console.log(e))
+// to get to the alert button
+
+// document.querySelector('#alert').addEventListener
+// ('click',alertMe)
+
+// //,()=> alert('Hi, I was clicked'))
+
+//  function alertMe(){
+//     return alert('Hi am cliked')
+//  }
+ document.querySelector('#alert').addEventListener
+('click',() => alert('Hi I was clicked'))
+
+//log something
+document.querySelector('#log').addEventListener
+('click', () => console.log('I\'ve been logged'))//show the statement on clicking the button on the browser
+
+//a callback to handle delete
+function handleDelete(e){
+    //console.log// 
+    //(e.target).innerText = 'Delete'//shows delete on clicking on x
+    //(e.target).innerHTML = ''//removes the HTML 
+        e.target.parentNode.remove()
+
+
+}
+
+//to get buttons and delete the parent node
+function addEventListenerToDeleteButtons(){
+    let buttons =  document.getElementsByClassName
+    ('delete_btn')
+    for(let btn of buttons){
+        //console.log(btn)
+        btn.addEventListener('click',handleDelete)
+    }
+}
+addEventListenerToDeleteButtons()
